500c2c916b1bcbe69b608f90064d7d3409e313b2
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-06 17:55:00 +0400

    Updated in 'Sharjah, AE' on BRANCH 'food-orders' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  controllers/index.ts models/index.ts routes/index.ts services/ExpressApp.ts

--- services/ExpressApp.ts
+++ services/ExpressApp.ts
@@ -1,9 +1,8 @@
 import express, { Application } from "express";
 import bodyParser from "body-parser";
-import { AdminRoute, VendorRoute } from "../routes";
+import { AdminRoute, VendorRoute, ShoppingRoute } from "../routes";
 import logger from "../utility/logger";
 import path from 'path';
-
 
 export default async(app: Application) => {
     app.use(bodyParser.json());
@@ -12,6 +11,7 @@ export default async(app: Application) => {
     app.use('/images', express.static(path.join(__dirname, 'images')));
     app.use('/admin', AdminRoute);
     app.use('/vendor', VendorRoute);
+    app.use('/shopping', ShoppingRoute)
 
     return app;
 }
